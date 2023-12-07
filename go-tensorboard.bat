d4d7276c2fa58b2071db766520973b6e4cc5f851
Aleksandr Akishin <102290527+AkishinAleksandr@users.noreply.github.com>
2023-12-07 16:37:42 +0400

    update file

---
+++ go-tensorboard.bat
@@ -0,0 +1,22 @@
+@echo off
+title Applio-RVC-Fork - Tensorboard
+
+rem Conda env variables
+set CONDA_ROOT_PREFIX=%UserProfile%\Miniconda3
+set INSTALL_ENV_DIR=%cd%\env
+
+echo Activating Conda environment...
+call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%"
+
+if errorlevel 1 (
+    echo Failed to activate Conda environment.
+    pause
+    exit /b 1
+)
+
+echo Starting the Tensorboard with Conda...
+cls
+echo Starting Tensorboard...
+python lib\fixes\tensor-launch.py
+pause
+exit /b 0
